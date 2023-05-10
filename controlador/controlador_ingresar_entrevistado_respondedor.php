f8c16719225a3e1fd9c69461b2bd2bf5ee8d899d
MateoBCoding <105530197+MateoBCoding@users.noreply.github.com>
2023-05-10 17:40:18 -0500

    Update controlador_ingresar_entrevistado_respondedor.php

--- controlador/controlador_ingresar_entrevistado_respondedor.php
+++ controlador/controlador_ingresar_entrevistado_respondedor.php
@@ -6,8 +6,11 @@
     $_SESSION["generoaso"] = $_POST["generoaso"];
     $_SESSION["relacionaso"] = $_POST["relacionaso"];
     
- //   $resultado = $conexion->query("insert into respondedor (idrespondet,nombreResponden,sexoresponden, telefonoresponden, id_relacion)
-   //         values ('$identificacion', '$nombre','$generoaso','$tefonoaso','$relacionaso')");
+ $resultado = $conexion->query("insert into respondedor (idrespondet,nombreResponden,sexoresponden, telefonoresponden, id_relacion)
+         values ('$identificacion', '$nombre','$generoaso','$tefonoaso','$relacionaso')");
 
+
+ $resultado = $conexion->query("insert into entrevistado (idrespondet,nombreResponden,sexoresponden, telefonoresponden, id_relacion)
+         values ('$identificacion', '$nombre','$generoaso','$tefonoaso','$relacionaso')");
 
 ?>
